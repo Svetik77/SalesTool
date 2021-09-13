2df4472e8c2d605333c861d1805d11ba61ce9398
Rimma77 <rimma34@gmail.com>
2021-09-13 21:10:05 +0200

    fixCaps/ fix case of greeting display

--- SalesTool/src/main/java/ca/demo/salestool/Main.java
+++ SalesTool/src/main/java/ca/demo/salestool/Main.java
@@ -10,9 +10,10 @@ public class Main {
 	}
 	
 	private static void displayGreeting() {
-		System.out.println("HELLO HAPPY SALES PEOPLE!");
+//		System.out.println("HELLO HAPPY SALES PEOPLE!");
+		System.out.println("---fixCaps branch---");
 		System.out.println("THIS APP SHOWS SALES DATA");
-		System.out.println("WOot! It pushed!");
+		System.out.println("----------------------");
 		
 		System.out.println("Test 1");
 		System.out.println("Test 1");
