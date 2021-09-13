d16ed8c6ea1f788b7a33fb0c3d155fc70fdc591c
Rimma77 <rimma34@gmail.com>
2021-09-13 21:04:59 +0200

    Partial implementation of loop

--- SalesTool/src/main/java/ca/demo/salestool/SalesData.java
+++ SalesTool/src/main/java/ca/demo/salestool/SalesData.java
@@ -4,6 +4,9 @@ public class SalesData {
 	private int data[] = {0, 4, 42};
 
 	public void display() {
+		for (int i : data) {
+			System.out.println("");
+		}
 		System.out.println("TODO: Implement data display");
 	}
 }
